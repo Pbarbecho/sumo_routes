fb1153613ef9ce1460ebc514bdee210961d51d0e
Pbarbecho <pablo.barbecho@upc.edu>
2020-12-07 16:20:05 +0100

    SUMO routes generation

---
+++ routes.py
@@ -0,0 +1,269 @@
+import os, sys
+import xml.etree.ElementTree as ET
+import multiprocessing
+import sumolib
+import pandas as pd
+import numpy as np
+import time
+import math
+from tqdm import tqdm
+from joblib import Parallel, delayed, parallel_backend
+from utils import SUMO_preprocess
+
+
+# number of cpus
+processors = multiprocessing.cpu_count()-18 # due to memory lack -> Catalunya  map = 2GB
+
+# Static paths
+sim_dir = '/root/Documents/SUMO_SEM/CATALUNYA/sim_files'   # directory of sumo cfg files
+routes_output = os.path.join(sim_dir, 'DUA')               # duarouter output files 
+sumo_cfg_output = os.path.join(sim_dir, 'SUMO')   
+simulation_outputs = os.path.join(sim_dir, '../outputs')   # simulation outputs folder   
+xmltocsv_dir = os.path.join(sim_dir,'..', 'xmltocsv') # xml directory
+parsed_dir = os.path.join(sim_dir,'..', 'parsed')
+
+# SUMO templeates
+o_dir = os.path.join(sim_dir, 'O')                         # O file location
+od2trips_conf = os.path.join(sim_dir,'od2trips.cfg.xml')   # od2trips.cfg file location     
+duarouter_conf = os.path.join(sim_dir,'duarouter.cfg.xml') # duaroter.cfg file location
+sumo_cfg = os.path.join(sim_dir, 'catalunya.sumo.cfg')
+
+# Informacion de los hospitales y distritos tal como aparece en TAZ file 
+hospitals = ['HospitalViladecans', 'HospitaldeBarcelona']
+sanitary_districs = ['Sitges', 'Barcelona', 'Terrasa','Vic']
+    
+# General settings
+veh_num = 80  # number of vehicles in O file
+n_repetitions = 15 # number of repetitions 
+sim_time = 48 # in hours # TO DO parameter of time in files
+factor = 100 # multiplied by the number of vehicles
+
+
+def gen_routes(O, k):
+    
+    # Generate od2trips cfg
+    cfg_name, output_name = gen_od2trips(O,k)
+    
+    # Execute od2trips
+    exec_od2trips(cfg_name)
+    
+    # Generate DUArouter cfg
+    cfg_name, output_name = gen_DUArouter(output_name, k)
+        
+    # Generate sumo cfg
+    gen_sumo_cfg(output_name, k)
+    
+    
+def gen_route_files():
+    print('\nGenerating cfg files\n')
+    for h in hospitals:
+        for sd in tqdm(sanitary_districs):
+            time.sleep(1)
+            
+            # build O file    
+            O_name = os.path.join(o_dir, f'{h}_{sd}')
+            create_O_file(O_name, f'{h}', f'{sd}', veh_num)
+            
+            # Generate cfg files 
+            for k in tqdm(range(n_repetitions)):
+                time.sleep(1) 
+                gen_routes(O_name, k)
+    
+    
+
+def create_O_file(fname, hospital, sanitary_distric, vehicles):
+    O = open(f"{fname}", "w")
+    text_list = ['$OR;D2\n',               # O format
+                 f'0.00 {sim_time}.00\n',  # Time 0-48 hours
+                 f'{factor}.00\n',         # Multiplication factor
+                 f'{hospital} '            # Origin
+             	 f'{sanitary_distric} ',   # Destination
+                 f'{vehicles}']            # NUmber of vehicles x multiplication factor
+    O.writelines(text_list)
+    O.close()
+
+
+    
+def gen_DUArouter(trips, i):
+    # Open original file
+    tree = ET.parse(duarouter_conf)
+    
+    # Update trip input
+    parent = tree.find('input')
+    ET.SubElement(parent, 'route-files').set('value', f'{trips}')    
+     
+    # Update output
+    parent = tree.find('output')
+    curr_name = os.path.basename(trips).split('_')
+    curr_name = curr_name[0] + '_' + curr_name[1]
+    output_name = os.path.join(routes_output, f'{curr_name}_dua_{i}.rou.xml')
+    ET.SubElement(parent, 'output-file').set('value', output_name)    
+    
+    # Update seed number
+    parent = tree.find('random_number')
+    ET.SubElement(parent, 'seed').set('value', f'{i}')    
+    
+    # Write xml
+    original_path = os.path.dirname(trips)
+    cfg_name = os.path.join(original_path, f'{curr_name}_duarouter_{i}.cfg.xml')
+    tree.write(cfg_name) 
+    return cfg_name, output_name
+        
+    
+    
+def gen_od2trips(O,k):
+    # Open original file
+    tree = ET.parse(od2trips_conf)
+    
+    # Update O input
+    parent = tree.find('input')
+    ET.SubElement(parent, 'od-matrix-files').set('value', f'{O}')    
+            
+    # Update output
+    parent = tree.find('output')
+    output_name = f'{O}_od2_{k}.trip.xml'
+    ET.SubElement(parent, 'output-file').set('value', output_name)    
+    
+    # Update seed number
+    parent = tree.find('random_number')
+    ET.SubElement(parent, 'seed').set('value', f'{k}')    
+    
+    # Write xml
+    cfg_name = f'{O}_trips_{k}.cfg.xml'
+    tree.write(cfg_name)
+    return cfg_name, output_name    
+
+
+def gen_sumo_cfg(dua, k):
+    # Open original file
+    tree = ET.parse(sumo_cfg)
+    
+    # Update rou input
+    parent = tree.find('input')
+    ET.SubElement(parent, 'route-files').set('value', f'{dua}')    
+            
+    # Update outputs
+    parent = tree.find('output')
+    curr_name = os.path.basename(dua).split('_')
+    curr_name = curr_name[0] + '_' + curr_name[1]
+    
+    # outputs 
+    outputs = ['fcd', 'vehroute', 'tripinfo']
+    for out in outputs:
+        ET.SubElement(parent, f'{out}-output').set('value', os.path.join(
+            simulation_outputs, f'{curr_name}_{out}_{k}.xml'))    
+     
+    # Write xml
+    output_dir = os.path.join(sumo_cfg_output, f'{curr_name}_{k}.sumo.cfg')
+    tree.write(output_dir)
+    
+    
+    
+def exec_od2trips(fname):
+    cmd = f'od2trips -c {fname}'
+    os.system(cmd)
+
+
+def exec_duarouter_cmd(fname):
+    cmd = f'duarouter -c {fname}'
+    os.system(cmd)    
+    
+    
+def exec_DUArouter():
+    cfg_files = os.listdir(o_dir)
+    
+    # Get dua.cfg files list
+    dua_cfg_list = []
+    [dua_cfg_list.append(cf) for cf in cfg_files if 'duarouter' in cf.split('_')]
+    
+    # Generate dua routes
+    if dua_cfg_list:
+        print(f'\nGenerating duaroutes ({len(dua_cfg_list)} files) ...........\n')
+        if dua_cfg_list: 
+            set_length = math.ceil(len(dua_cfg_list)/processors)
+            # Parallelize files generation
+            with parallel_backend("loky"):
+                Parallel(n_jobs=processors, verbose=0, batch_size=int(set_length))(delayed(exec_duarouter_cmd)(
+                     os.path.join(o_dir, cfg)) for cfg in dua_cfg_list)
+    else:
+       sys.exit('No dua.cfg files}')
+    
+
+def summary():
+    # Count generated files
+    output_files = os.listdir(routes_output)   
+    expected_files = len(hospitals)*len(sanitary_districs)*n_repetitions
+    generated_files = len(output_files)/2 # /2 due to alt files
+    print(f'\nExpected files: {expected_files}   Generated files: {generated_files}\n')
+    if generated_files != expected_files:
+        print('Missing files, check log at console')
+    else:
+        # Count routes 
+        measure = ['id','fromTaz', 'toTaz']
+        out_list = []
+        for f in output_files:    
+            if 'alt' not in f.split('.'):
+                summary_list = sumolib.output.parse_sax__asList(os.path.join(routes_output,f), "vehicle", measure)
+                temp_df = pd.DataFrame(summary_list).groupby(['fromTaz', 'toTaz']).count().reset_index()
+                temp_df['Repetition'] = f.split('.')[0].split('_')[-1]
+                out_list.append(temp_df.to_numpy()[0])
+                
+        summary = pd.DataFrame(out_list, columns=['Hospital', 'Accident','Routes', 'Repetition']).sort_values(by=['Repetition', 'Hospital', 'Accident'])
+        save_to = os.path.join(sim_dir,'route_files.csv')
+        summary.to_csv(save_to, index=False, header=True)
+        print(summary)
+        print(f'\nSummary saved to: {save_to}\n')
+        
+        
+def clean_memory():
+    #Clean memory cache at the end of simulation execution
+    if os.name != 'nt':  # Linux system
+        os.system('sync')
+        os.system('echo 3 > /proc/sys/vm/drop_caches')
+        os.system('swapoff -a && swapon -a')
+    # print("Memory cleaned")
+    
+    
+def simulate():
+    simulations = os.listdir(sumo_cfg_output)
+    if simulations: 
+        set_length = len(simulations)/processors
+        with parallel_backend("loky"):
+                Parallel(n_jobs=processors, verbose=0, batch_size=int(set_length))(delayed(exec_sim_cmd)(s) for s in simulations)
+        clean_memory()
+    else:
+       sys.exit('No sumo.cfg files}')
+  
+                
+       
+def exec_sim_cmd(cfg_file):
+    full_path = os.path.join(sumo_cfg_output, cfg_file)
+    cmd = f'sumo -c {full_path}'
+    os.system(cmd)
+  
+    
+  
+def SUMO_outputs_process():
+    class options:
+        sumofiles = simulation_outputs
+        xmltocsv = xmltocsv_dir
+        parsed = parsed_dir
+    data = SUMO_preprocess(options)
+    
+      
+    
+# Generate cfg files
+#gen_route_files()
+
+# Execute DUArouter 
+exec_DUArouter()
+            
+        
+# Execute simulations
+summary()
+
+# Exec simulations
+simulate()     
+
+# Outputs preprocess
+SUMO_outputs_process()
---
+++ utils.py
@@ -0,0 +1,165 @@
+import pandas as pd
+import sys, os
+from tqdm import tqdm
+from joblib import Parallel, delayed, parallel_backend
+import math
+import time
+import multiprocessing
+
+
+# import sumo tool xmltocsv
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join('/opt/sumo-1.5.0/', 'tools')
+    curr_dir = os.path.dirname(os.path.realpath(__file__))
+    sys.path.append(os.path.join(tools))
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+    
+
+
+def SUMO_preprocess(options):
+    # Process SUMO output files to build the dataset
+    # no memmory requiremetns
+    processors = multiprocessing.cpu_count()-2
+   
+    def save_file(df, name, parsed_dir):
+        #print(f'Parsed --> {name}')
+        if 'ID' in df.keys():
+            df.sort_values(by=['ID'], inplace=True)
+        df.to_csv(os.path.join(parsed_dir, f'{name}.csv'), index=False, header=True)
+        
+    
+    def parallelxml2csv(f, options):
+        time.sleep(1)
+        # output directory
+        output = os.path.join(options.xmltocsv, f'{f.strip(".xml")}.csv')
+        # SUMO tool xml into csv
+        sumo_tool = os.path.join(tools, 'xml', 'xml2csv.py')
+        # Run sumo tool with sumo output file as input
+        cmd = 'python {} {} -s , -o {}'.format(sumo_tool, os.path.join(options.sumofiles,f), output)
+        os.system(cmd)
+
+    def xml2csv(options): 
+        # convert xml to csv
+        files_list = os.listdir(options.sumofiles)
+        set_length = math.ceil(len(files_list)/processors)
+       
+        if files_list:
+            print(f'\nGenerating {len(files_list)} csv files. This may take a while .........')
+            # Parallelize files generation
+            with parallel_backend("loky"):
+                Parallel(n_jobs=processors, verbose=0, batch_size=int(set_length))(delayed(parallelxml2csv)(
+                     f, options) for f in tqdm(files_list))
+        else:
+            sys.exit(f"Empty or missing output data files: {options.sumofiles}")
+        
+        # Read generated csvs
+        csvs_list = os.listdir(options.xmltocsv)
+        if len(csvs_list) == len(files_list):
+            data_list = []
+            print(f'\nBuilding {len(csvs_list)} dataframes from sumo outputs ......\n')
+            for csv in tqdm(csvs_list):
+                time.sleep(1)
+                dname = csv.split('.')[0].split('_')
+                data = pd.read_csv(os.path.join(options.xmltocsv, csv))
+                dname.append(data)
+                data_list.append(dname)
+            result_df = pd.DataFrame(data_list, columns=['Hospital', 'Accident', 'Output','Repetition','Dataframe'])
+            
+            nfiles = len(result_df.index)
+            efiles = nfiles/len(result_df["Output"].unique()) # total files / number of output sumo (tripinfo, fcd, vehroute)
+            print(f'\nReading {nfiles} files. Expected {efiles} parsed files. This may take a while .........')
+            # Parallelize parse files
+            with parallel_backend("loky"):
+                Parallel(n_jobs=processors, verbose=0, batch_size=int(set_length))(delayed(parallel_parse_output_files)(
+                     key, group_df) for key, group_df in result_df.groupby(['Hospital', 'Accident', 'Repetition']))
+            print(f'Generated: {len(os.listdir(options.parsed))} parsed files')
+        else:
+            sys.exit(f'Missing csv files: {options.xmltocsv}')
+
+ 
+           
+    def merge_files(options):
+        #combine all files in the parsed dir
+        parsed_files = os.listdir(options.parsed)
+        print(f'\nCombining {len(parsed_files)} files')
+        combined_csv = pd.concat([pd.read_csv(os.path.join(options.parsed, pf)) for pf in parsed_files ])
+        #export to csv
+        combined_csv.to_csv(os.path.join(options.parsed, "data.csv"), index=False, header=True)
+        return combined_csv
+                
+     
+        
+        
+    def veh_trip_info(df):
+        # filter know features
+        df = df.filter(items=['tripinfo_duration', 
+                              'tripinfo_routeLength', 
+                              'tripinfo_timeLoss', 
+                              'tripinfo_waitingCount', 
+                              'tripinfo_waitingTime', 
+                              'tripinfo_arrivalLane', 
+                              'tripinfo_departLane',
+                              'tripinfo_id']).rename(columns={'tripinfo_id':'ID'})
+        return df
+    
+                
+    def parallel_parse_output_files(key, group_df):
+        # save parsed files
+        df_name = f'{key[0]}_{key[1]}_{key[2]}'
+        # Process sumo outputs
+        vehroute = group_df.loc[group_df['Output'] == 'vehroute', 'Dataframe'].iloc[0]
+        fcd = group_df.loc[group_df['Output'] == 'fcd', 'Dataframe'].iloc[0]
+        tripinfo = group_df.loc[group_df['Output'] == 'tripinfo', 'Dataframe'].iloc[0]
+        taz_locations_edgenum_df = lanes_counter_taz_locations(vehroute)                  # Count edges on route from vehroute file and get from/to TAZ locations
+        veh_speed_positions_df = avrg_speed_and_geo_positions(fcd)                        # Get average speed and initial/end positions (x,y)
+        tripinfo_df = veh_trip_info(tripinfo) 
+        # merge dataframes
+        sdata = taz_locations_edgenum_df.merge(veh_speed_positions_df,on='ID').merge(tripinfo_df,on='ID')
+        # save each scenario in parsed files
+        save_file(sdata, f'{df_name}', options.parsed)
+						
+											      
+        
+    def lanes_counter_taz_locations(df):
+        # contador de edges en ruta
+        df['lane_count'] = df['route_edges'].apply(lambda x: len(x.split()))
+        df = df.filter(items=['vehicle_id', 'lane_count', 'vehicle_fromTaz', 'vehicle_toTaz']).rename(columns={'vehicle_id':'ID'})
+        return df
+
+
+    def get_positions(df, id,min,max):
+        # get initial and end positions based on ini/end time of vehicle id
+        ini_pos =  df.loc[(df['vehicle_id'] == id) & (df['timestep_time'] == min)].iloc[0]
+        end_pos =  df.loc[(df['vehicle_id'] == id) & (df['timestep_time'] == max)].iloc[0]
+        return id, min, max, ini_pos.vehicle_x, ini_pos.vehicle_y, end_pos.vehicle_x, end_pos.vehicle_y 
+        
+       
+    def avrg_speed_and_geo_positions(df):
+        # get average speed
+        speed_df = df.groupby(['vehicle_id']).mean().reset_index()
+        speed_df = speed_df.filter(items=['vehicle_id','vehicle_speed']).rename(columns={'vehicle_id':'ID','vehicle_speed':'avrg_speed'})
+        # Prepare df with ini end times of vehicles 
+        df = df.filter(items=['vehicle_id', 'timestep_time','vehicle_x', 'vehicle_y'])
+        df.dropna(subset = ["vehicle_id"], inplace=True)
+        # get initial end times of vechiel
+        time_df = df.groupby(['vehicle_id']).timestep_time.agg(['min','max']).reset_index()
+        # Get positions df
+        positions_list = [get_positions(df,id,min,max) for id,min,max in zip(time_df['vehicle_id'], time_df['min'], time_df['max'])]
+        positions_df = pd.DataFrame(positions_list, columns=['ID', 'ini_time', 'end_time', 'ini_x_pos', 'ini_y_pos','end_x_pos', 'end_y_pos'])
+        # Merge speed and positions df
+        speed_and_positions = speed_df.merge(positions_df, on='ID')
+        return speed_and_positions
+    
+    
+    xml2csv(options)                                                                   # Get sumo output files into a dictionary of dataframes
+    merge_files(options)
+    
+    
+    
+    
+    
+    
+    
+    
